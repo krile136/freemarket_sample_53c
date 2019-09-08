b1bf892883e5d4aa2be26e250f5d58568c55bdcb
Hiroyuki Matsuoka <hiroyukimatsuoka@example.com>
2019-09-08 22:55:37 +0900

    ganerated phone authentication page

--- app/assets/javascripts/registration.js
+++ app/assets/javascripts/registration.js
@@ -1,9 +1,22 @@
 $(document).on('turbolinks:load', function () {
-  $('#registration_one').on("click", function () {
+  // 会員情報の「次へ進む」をクリックした時
+  $('#user-info-registration').on("click", function () {
+    // 会員情報登録の内容を非表示
     $('#member-information').css('display', 'none');
+    // プログレスバーの色を変更
     $('#through .progress-status_bar').css('background', '#ea352d');
     $('#active').css('color', '#ea352d');
     $('#active .progress-status').css('background', '#ea352d');
+    // 電話番号確認画面の表示
     $('#registration-title').text("電話番号の確認");
+    $('#confirm-phone-number').css('display', 'block');
+  });
+
+  $('#user-phone-registration').on("click", function () {
+    // 電話番号確認ページの非表示
+    $('#confirm-phone-number').css('display', 'none');
+    // 電話番号認証画面の表示
+    $('#registration-title').text("電話番号認証");
+    $('#phone-number-authentication').css('display', 'block');
   });
 });
