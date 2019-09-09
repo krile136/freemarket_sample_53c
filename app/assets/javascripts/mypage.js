7c0e11c14c02b7d0d7fad4c4af280122382ca8f6
kor-901 <52075363+kor-901@users.noreply.github.com>
2019-09-10 01:45:17 +0900

    Add js about switch tab and lists

---
+++ app/assets/javascripts/mypage.js
@@ -0,0 +1,46 @@
+$(function() {
+
+  // タブクリック時の表示切り替え処理
+  $('.mypage-content__main__tablist__header__tab').on('click', function () {
+    let clickedTab = $(this);
+
+    // クリックしたタブがアクティブでない場合のみ処理続行
+    if (!clickedTab.hasClass('mypage-content__main__tablist__header__tab-active')) {
+      let pairTab = '';
+      let shownList = '';
+      let hiddenList = '';
+
+      switch (clickedTab.attr('id')) {
+        case 'mypage-notification-tab':
+            pairTab = $('#mypage-todo-tab');
+            shownList = $('#mypage-notification-list');
+            hiddenList = $('#mypage-todo-list');
+            break;
+        case 'mypage-todo-tab':
+            pairTab = $('#mypage-notification-tab');
+            shownList = $('#mypage-todo-list');
+            hiddenList = $('#mypage-notification-list');
+            break;
+        case 'mypage-purchase-tab':
+            pairTab = $('#mypage-purchased-tab');
+            shownList = $('#mypage-purchase-list');
+            hiddenList = $('#mypage-purchased-list');
+            break;
+        case 'mypage-purchased-tab':
+            pairTab = $('#mypage-purchase-tab');
+            shownList = $('#mypage-purchased-list');
+            hiddenList = $('#mypage-purchase-list');
+      }
+      // クリックしたタブをアクティブにする
+      clickedTab.removeClass('mypage-content__main__tablist__header__tab-inactive');
+      clickedTab.addClass('mypage-content__main__tablist__header__tab-active');;
+      // クリックしたタブの隣のタブを非アクティブにする
+      pairTab.removeClass('mypage-content__main__tablist__header__tab-active');
+      pairTab.addClass('mypage-content__main__tablist__header__tab-inactive');
+      // アクティブにしたタブ配下のリストを表示
+      shownList.removeClass('mypage-content__main__tablist-inactive');
+      // 非アクティブにしたタブ配下のリストを非表示
+      hiddenList.addClass('mypage-content__main__tablist-inactive');
+    }
+  });
+});
