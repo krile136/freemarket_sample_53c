6fbf42bbd5d21f21f2edc1573f88477ad63e24e5
Hiroyuki Matsuoka <hiroyukimatsuoka@example.com>
2019-09-16 23:47:30 +0900

    generated login page and refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'users#new'
+  # root 'user#new'
+  resources :users
+  devise_scope :user do
+    root :to => "devise/sessions#new"
+  end
 end
