da24b0d17a9684143e3a95edcbbff29e87eed005
kor-901 <52075363+kor-901@users.noreply.github.com>
2019-09-16 15:39:54 +0900

    Add routing of users-edit view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'items#index'
-  resources :users, only: [:show]    
+  resources :users, only: [:show, :edit]
   resources :items, only: [:index, :show]
 end
