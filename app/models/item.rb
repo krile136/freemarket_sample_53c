e138a0413681203e030a255e2337e350e961c503
takumi14 <03serious@gmail.com>
2019-10-01 14:12:26 +0900

    image-upload-function

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,7 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
+    has_many :images, dependent: :destroy
+    accepts_nested_attributes_for :images
     belongs_to_active_hash :prefecture
     belongs_to_active_hash :condition
     
