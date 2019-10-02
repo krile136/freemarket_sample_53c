d177eeb4f4397e85ccce617acf97216434b06ff4
takumi14 <03serious@gmail.com>
2019-10-02 23:00:49 +0900

    create active_hash model
    active_hashで配送料・配送の方法・配送までの日数
    のモデルを作成。viewに表示させました。

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,11 @@ class Item < ApplicationRecord
     accepts_nested_attributes_for :images
     belongs_to_active_hash :prefecture
     belongs_to_active_hash :condition
-    
+    belongs_to_active_hash :postage_burden
+    belongs_to_active_hash :delivery_day
+    belongs_to_active_hash :delivery_method
     belongs_to :seller, class_name: 'User'
     belongs_to :buyer, class_name: 'User', optional: true
+
+    
 end
---
+++ db/migrate/20191001064033_remove_condition_id_to_items_condition_id.rb
@@ -0,0 +1,4 @@
+class RemoveConditionIdToItemsConditionId < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
