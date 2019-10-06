ea39f038160a8b1493cd22a316cc1bc31a140a3e
Hiroyuki Matsuoka <hiroyukimatsuoka@example.com>
2019-10-06 14:33:02 +0900

    generated function which edit user nickname and profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,11 +7,28 @@ class UsersController < ApplicationController
   def show
   end
 
+  def edit
+    user = User.find(params[:id])
+    @nickname = user.nickname
+    @profile = user.profile
+  end
+
+  def update
+    user = User.find(params[:id])
+    if user.id == current_user.id
+      user.update(user_edit_params)
+    end
+  end
+
   def complete
   end
 
   private
   def set_action_name
     @action_name = action_name
+  end
+
+  def user_edit_params
+    params.permit(:nickname, :profile)
   end
 end
