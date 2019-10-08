ad71ee14e7992af7d3a9d049debabcd33f576a95
takumi14 <03serious@gmail.com>
2019-10-08 14:29:35 +0900

    Generate categry model
    カテゴリーモデルの作成

--- db/migrate/20191001064033_remove_condition_id_to_items_condition_id.rb
+++
@@ -1,4 +0,0 @@
-class RemoveConditionIdToItemsConditionId < ActiveRecord::Migration[5.2]
-  def change
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_01_064033) do
+ActiveRecord::Schema.define(version: 2019_10_08_050846) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "prefecture_id"
     t.string "city"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
