c93468ad2bf0e5f183b3b429a1d76a46b618de2d
takumi14 <03serious@gmail.com>
2019-10-08 14:33:44 +0900

    Add ancestry column to category table
    categoriesテーブルにancestryのカラムを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_08_050846) do
+ActiveRecord::Schema.define(version: 2019_10_08_053104) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "prefecture_id"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 2019_10_08_050846) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "ancestry"
+    t.index ["ancestry"], name: "index_categories_on_ancestry"
   end
 
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
