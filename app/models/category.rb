11a5ce8510252deecf149aab84aef37d48ad13a6
takumi14 <03serious@gmail.com>
2019-10-09 15:49:30 +0900

    create size category_size model
    カテゴリーのidとサイズ（親）のidを結びつける中間テーブル（category_sizesテーブル）を作成する

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,7 @@
 class Category < ApplicationRecord
+  has_many :items
+  has_many :category_sizes
+  has_many :sizes, through: :category_sizes
   has_ancestry
   scope :get_parents,  -> { where(ancestry: nil) }
 end
