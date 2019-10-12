90b4a3422ae271a5f03f7431644a4b66245ef0b3
Hiroyuki Matsuoka <hiroyukimatsuoka@example.com>
2019-10-12 22:49:07 +0900

    generated rooting when creditcard registration failed

--- app/controllers/creditcards_controller.rb
+++ app/controllers/creditcards_controller.rb
@@ -1,13 +1,16 @@
 class CreditcardsController < ApplicationController
 
   def create
-    # @comment = Comment.create(text: comment_params[:text], tweet_id: comment_params[:tweet_id], user_id: current_user.id)
-    @creditcard = Creditcard.create(credit_number: creditcard_params[:credit_number],
+    @creditcard = Creditcard.new(credit_number: creditcard_params[:credit_number],
                                     limit_month: creditcard_params[:limit_month], 
                                     limit_year: creditcard_params[:limit_year],
                                     security_number: creditcard_params[:security_number],
                                     user_id: creditcard_params[:user_id])
-    redirect_to "/users/#{current_user.id}/show_creditcard"
+    if @creditcard.save
+      redirect_to "/users/#{current_user.id}/show_creditcard"
+    else
+      redirect_to "/users/#{current_user.id}/new_creditcard", alert: '入力情報に間違いがあります'
+    end
   end
 
 
