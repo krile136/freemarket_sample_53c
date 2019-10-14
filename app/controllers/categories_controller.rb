c346e997148827980ac542a6b322ce83021b12a8
kor-901 <52075363+kor-901@users.noreply.github.com>
2019-10-14 19:30:44 +0900

    Fix indent

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,7 +2,7 @@ class CategoriesController < ApplicationController
   def set_children
     @children = Category.find(params[:parent_id]).get_children
     respond_to do |format|
-			format.json
-		end
+      format.json
+    end
   end
 end
