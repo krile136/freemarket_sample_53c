0fab60bd698f4a51460e8160231621362dfca656
takumi14 <03serious@gmail.com>
2019-10-14 22:23:16 +0900

    item model validateion
    itemモデルのバリデーション
    単体テスト実施。

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,21 @@ class Item < ApplicationRecord
     belongs_to_active_hash :postage_burden
     belongs_to_active_hash :delivery_day
     belongs_to_active_hash :delivery_method
-    belongs_to_active_hash :size, optional: true
+    belongs_to_active_hash :size
+    belongs_to :brand, optional: true
+    belongs_to :category
     belongs_to :seller, class_name: 'User'
     belongs_to :buyer, class_name: 'User', optional: true
+
+    validates :name, presence: true, length: { maximum: 40 }
+    validates :description, presence: true, length: { maximum: 1000 }
+    validates :price, presence: true, numericality: { only_integer: true , greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
+    validates :condition_id, presence: true
+    validates :postage_burden_id, presence: true
+    validates :prefecture_id, presence: true
+    validates :delivery_method_id, presence: true
+    validates :category_id, presence: true
+    validates :delivery_day_id, presence: true
+    validates :parent_id, presence: true
+    validates :child_id, presence: true
 end
---
+++ spec/factories/items.rb
@@ -0,0 +1,19 @@
+FactoryBot.define do
+  factory :item do
+    id                    {nil}
+    seller_id             {1}
+    buyer_id              {nil}
+    name                  {"testtesttest"}
+    description           {"testtest"}
+    price                 {2000}
+    condition_id          {1}
+    postage_burden_id     {1}
+    delivery_method_id    {1}
+    category_id           {19}
+    size_id               {1}
+    prefecture_id         {1}
+    delivery_day_id       {1}
+    parent_id             {1}
+    child_id              {2}
+  end
+end
