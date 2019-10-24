4895dab02067168c938546b31eaec30a49f55b10
kor-901 <52075363+kor-901@users.noreply.github.com>
2019-10-25 00:00:57 +0900

    Define decorator about creditcard

--- app/decorators/creditcard_decorator.rb
+++ app/decorators/creditcard_decorator.rb
@@ -1,13 +1,12 @@
 class CreditcardDecorator < Draper::Decorator
   delegate_all
 
-  # Define presentation-specific methods here. Helpers are accessed through
-  # `helpers` (aka `h`). You can override attributes, for example:
-  #
-  #   def created_at
-  #     helpers.content_tag :span, class: 'time' do
-  #       object.created_at.strftime("%a %m/%d/%y")
-  #     end
-  #   end
+  def masking_number
+    "************#{object.credit_number.slice(12, 4)}"
+  end
+
+  def limit_year_and_month
+    "#{sprintf("%02d", object.limit_month)} / #{object.limit_year % 100} "
+  end
 
 end
