7929534d438f5a2a87d4f7d77437d7868502ea2b
Hiroyuki Matsuoka <hiroyukimatsuoka@example.com>
2019-10-30 17:46:18 +0900

    marked up myitem page

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -37,6 +37,21 @@ class ItemsController < ApplicationController
       redicret_to user_items_path, alert: '出品に失敗しました。必須項目を確認してください。'
     end
   end
+
+  def myitem
+    @item = Item.find_by(id: params[:id])
+    @image_path = @item.images.map{|img| img.image_path}
+    @user = User.find(@item.seller_id)
+    @category_parent = Category.find(@item.parent_id).name
+    @category_child = Category.find(@item.child_id).name
+    @category_grandchild = Category.find(@item.category_id).name
+    @condition = Condition.find(@item.condition_id).name
+    @postage_burden = PostageBurden.find(@item.postage_burden.id).name
+    @delivery_method = DeliveryMethod.find(@item.delivery_method_id).name
+    @prefecture = Prefecture.find(@item.prefecture_id).name
+    @delivery_day = DeliveryDay.find(@item.delivery_day_id).name
+    @price = @item.price_separate
+  end
   
   def get_category_children
     @category_children = Category.find(params[:parent_id]).children
