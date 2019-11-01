6e43d6cf79090f374c3197ac4675f840173c9df8
Hiroyuki Matsuoka <hiroyukimatsuoka@example.com>
2019-11-01 13:11:57 +0900

    fiexed s3 address separating

--- app/models/image.rb
+++ app/models/image.rb
@@ -5,7 +5,7 @@ class Image < ApplicationRecord
 
   def image_path
     if Rails.env.production?
-      self.image_url[0].url.delete!('%5B%22').delete!('%22%5D"')
+      self.image_url[0].url.gsub(/%5B%22/, "").gsub(/%22%5D/, "")
     else
       self.image_url[0].file.file.sub(/.*public/,'').sub(/\["/,'').sub(/"\]/,'')
     end  
